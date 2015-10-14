ff4270fd5d9d79adc58dd9c1ff57546095144637
MergeCells <arashimata@gmail.com>
2015-10-15 00:17:58 +0900

    delay()を使わないコードに変更・コードの自動整形

--- encorder_zenbunose/encorder_zenbunose.ino
+++ encorder_zenbunose/encorder_zenbunose.ino
@@ -1,7 +1,7 @@
 //PCからのリクエストに対し前回の送信からの積算距離を送る
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(7,8,9,10,11,12);
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 #define DI_WHEEL_LEFT_A 14
 #define DI_WHEEL_LEFT_B 15
@@ -31,12 +31,14 @@ int ret;
 #define TIMEOUT 1000
 
 long now_time, before_time;
-double left_dist,right_dist,dist,odo;  //片輪の移動距離
-double wheel_speed_left,wheel_speed_right,wheel_speed;  //片輪の速度/中心の速度
+double left_dist, right_dist, dist, odo; //片輪の移動距離
+double wheel_speed_left, wheel_speed_right, wheel_speed; //片輪の速度/中心の速度
 double wheel_rotation;  //回転角
 double rotation_radius; //回転半径
 
 String strData;
+
+long updateTime = 0; // 更新時刻
 
 int wheelState, prevState, prevLeft, prevRight;
 #define ST_WHL_NONE 0x0
@@ -55,121 +57,121 @@ void setup() {
   lcd.print("START"); /* 文字列の表示 */
   delay(1000);
   lcd.clear();                /* LCDのクリア */
-  
-  pinMode(DO_LED_STAT, OUTPUT);  
-  pinMode(DO_LED_ERR, OUTPUT);  
 
-  pinMode(DI_WHEEL_LEFT_A, INPUT);     
+  pinMode(DO_LED_STAT, OUTPUT);
+  pinMode(DO_LED_ERR, OUTPUT);
+
+  pinMode(DI_WHEEL_LEFT_A, INPUT);
   pinMode(DI_WHEEL_LEFT_B, INPUT);
-  pinMode(DI_WHEEL_RIGHT_A, INPUT);     
+  pinMode(DI_WHEEL_RIGHT_A, INPUT);
   pinMode(DI_WHEEL_RIGHT_B, INPUT);
 
-  pinMode(SWITCH_CHECK,INPUT);
+  pinMode(SWITCH_CHECK, INPUT);
 
-  
- // pinMode(AI_ROTARY_ENCODER_L, INPUT);
- //pinMode(AI_ROTARY_ENCODER_R, INPUT);
-  
+
+  // pinMode(AI_ROTARY_ENCODER_L, INPUT);
+  //pinMode(AI_ROTARY_ENCODER_R, INPUT);
+
   cnt_left = cnt_right = 0;
-//  tm_serial_out = 0;
-  
-  wheelState &= digitalRead(DI_WHEEL_LEFT_A) | digitalRead(DI_WHEEL_LEFT_B)<<1;
+  //  tm_serial_out = 0;
+
+  wheelState &= digitalRead(DI_WHEEL_LEFT_A) | digitalRead(DI_WHEEL_LEFT_B) << 1;
   prevState = wheelState;
-  
+
   Serial.begin(9600);
 }
 
-void loop(){
-  wheelState = digitalRead(DI_WHEEL_LEFT_A) | digitalRead(DI_WHEEL_LEFT_B)<<1 | digitalRead(DI_WHEEL_RIGHT_A)<<2 | digitalRead(DI_WHEEL_RIGHT_B)<<3;
+void loop() {
+  wheelState = digitalRead(DI_WHEEL_LEFT_A) | digitalRead(DI_WHEEL_LEFT_B) << 1 | digitalRead(DI_WHEEL_RIGHT_A) << 2 | digitalRead(DI_WHEEL_RIGHT_B) << 3;
   //enc_left = analogRead(AI_ROTARY_ENCODER_L);
   //enc_right = analogRead(AI_ROTARY_ENCODER_R);
-  
-  prevLeft = prevState&ST_WHL_LEFT;
-  prevRight = prevState&ST_WHL_RIGHT;
-  
+
+  prevLeft = prevState & ST_WHL_LEFT;
+  prevRight = prevState & ST_WHL_RIGHT;
+
   // LEFT WHEEL COUNTER UPDATE
-  switch(wheelState&ST_WHL_LEFT){
+  switch (wheelState & ST_WHL_LEFT) {
     case ST_WHL_LEFT:
-      if(prevLeft==ST_WHL_LA){
+      if (prevLeft == ST_WHL_LA) {
         //Serial.println((char)0b01);
         cnt_left++;
-      }else if(prevLeft==ST_WHL_LB){
+      } else if (prevLeft == ST_WHL_LB) {
         //Serial.println((char)0b00);
         cnt_left--;
       }
       break;
     case ST_WHL_LA:
-      if(prevLeft==ST_WHL_NONE){
+      if (prevLeft == ST_WHL_NONE) {
         //Serial.println((char)0b01);
         cnt_left++;
-      }else if(prevLeft==ST_WHL_LEFT){
+      } else if (prevLeft == ST_WHL_LEFT) {
         //Serial.println((char)0b00);
         cnt_left--;
       }
       break;
     case ST_WHL_NONE:
-      if(prevLeft==ST_WHL_LB){
+      if (prevLeft == ST_WHL_LB) {
         //Serial.println((char)0b01);
         cnt_left++;
-      }else if(prevLeft==ST_WHL_LA){
+      } else if (prevLeft == ST_WHL_LA) {
         //Serial.println((char)0b00);
         cnt_left--;
       }
       break;
     case ST_WHL_LB:
-      if(prevLeft==ST_WHL_LEFT){
+      if (prevLeft == ST_WHL_LEFT) {
         //Serial.println((char)0b01);
         cnt_left++;
-      }else if(prevLeft==ST_WHL_NONE){
+      } else if (prevLeft == ST_WHL_NONE) {
         //Serial.println((char)0b00);
         cnt_left--;
       }
       break;
   }
-  
-  switch(wheelState&ST_WHL_RIGHT){
+
+  switch (wheelState & ST_WHL_RIGHT) {
     case ST_WHL_RIGHT:
-      if(prevRight==ST_WHL_RA){
+      if (prevRight == ST_WHL_RA) {
         //Serial.println((char)0b11);
         cnt_right++;
-      }else if(prevRight==ST_WHL_RB){
+      } else if (prevRight == ST_WHL_RB) {
         //Serial.println((char)0b10);
         cnt_right--;
       }
       break;
     case ST_WHL_RA:
-      if(prevRight==ST_WHL_NONE){
+      if (prevRight == ST_WHL_NONE) {
         //Serial.println((char)0b11);
         cnt_right++;
-      }else if(prevRight==ST_WHL_RIGHT){
+      } else if (prevRight == ST_WHL_RIGHT) {
         //Serial.println((char)0b10);
         cnt_right--;
       }
       break;
     case ST_WHL_NONE:
-      if(prevRight==ST_WHL_RB){
+      if (prevRight == ST_WHL_RB) {
         //Serial.println((char)0b11);
         cnt_right++;
-      }else if(prevRight==ST_WHL_RA){
+      } else if (prevRight == ST_WHL_RA) {
         //Serial.println((char)0b10);
         cnt_right--;
       }
       break;
     case ST_WHL_RB:
-      if(prevRight==ST_WHL_RIGHT){
+      if (prevRight == ST_WHL_RIGHT) {
         //Serial.println((char)0b11);
         cnt_right++;
-      }else if(prevRight==ST_WHL_NONE){
+      } else if (prevRight == ST_WHL_NONE) {
         //Serial.println((char)0b10);
         cnt_right--;
       }
       break;
   }
-  prevState=wheelState;
+  prevState = wheelState;
 
-// ret = Serial.read();
+  // ret = Serial.read();
 
-  if(Serial.read() > 0)
+  if (Serial.read() > 0)
   {
     Serial.write(cnt_left);
     Serial.write(cnt_right);
@@ -177,26 +179,33 @@ void loop(){
     cnt_right = 0;
   }
 
-if(digitalRead(SWITCH_CHECK) == HIGH){
-    
-    lcd.setCursor(0,0);
-    lcd.print(analogRead(0));
-    lcd.setCursor(4,0);
-    lcd.print(analogRead(1));
-    lcd.setCursor(0,1);
-    lcd.print(analogRead(2));
-    lcd.setCursor(4,1);
-    lcd.print(analogRead(3));
-    delay(200);   
+  if (digitalRead(SWITCH_CHECK) == HIGH &&
+      millis() / 200 != updateTime) {
+
+    // 更新時刻（整数型）を記憶し，次の単位時刻になるとTrue
+    // 次の単位時刻になったとき，更新時刻に代入し直す
+    updateTime = millis() / 200;
+
     lcd.clear();
+
+    lcd.setCursor(0, 0);
+    lcd.print(analogRead(0));
+    lcd.setCursor(4, 0);
+    lcd.print(analogRead(1));
+
+    lcd.setCursor(0, 1);
+    lcd.print(analogRead(2));
+    lcd.setCursor(4, 1);
+    lcd.print(analogRead(3));
+    // delay(200);
   }
 
 
   // LED
-  if (wheelState& (ST_WHL_LA|ST_WHL_RA)) {     
-    digitalWrite(DO_LED_STAT, HIGH);  
+  if (wheelState & (ST_WHL_LA | ST_WHL_RA)) {
+    digitalWrite(DO_LED_STAT, HIGH);
     //Serial.println("a");
-  }else{
+  } else {
     digitalWrite(DO_LED_STAT, LOW);
     //Serial.println("b");
   }
